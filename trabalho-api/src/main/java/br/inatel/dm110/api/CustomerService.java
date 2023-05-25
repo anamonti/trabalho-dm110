58971bd3918d5ff1254f02ca88643f0950d8094a
Ana Julia Monti <anajuliamonti8@gmail.com>
2023-05-24 22:36:05 -0300

    Create api and rest classes

---
+++ trabalho-api/src/main/java/br/inatel/dm110/api/CustomerService.java
@@ -0,0 +1,34 @@
+package br.inatel.dm110.api;
+
+import java.util.Collection;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+
+@Path("/customer")
+public interface CustomerService {
+	@POST
+	@Path("/add")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void addCustomer(CustomerTO customer);
+	
+	@GET
+	@Path("/get/{cpf}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public CustomerTO getCustomer(@PathParam("cpf") String cpf);
+	
+	@GET
+	@Path("/get")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Collection<CustomerTO> getCustomers();
+	
+	@POST
+	@Path("/update")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void updateCustomer();
+}
---
+++ trabalho-impl/src/main/java/br/inatel/dm110/impl/CustomerServiceImpl.java
@@ -0,0 +1,34 @@
+package br.inatel.dm110.impl;
+
+import javax.enterprise.context.RequestScoped;
+
+import br.inatel.dm110.api.CustomerService;
+
+@RequestScoped
+public class CustomerServiceImpl implements CustomerService {
+
+	@Override
+	public void addCustomer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getCustomer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getCustomers() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateCustomer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
